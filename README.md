b08e558c3369ca532c5896ae39a318c7d41e73db
Ville Vuorenmaa <villevuor@gmail.com>
2017-11-24 18:28:53 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# crispy-umbrella
-Patut at Junction 2017
+# Pat
+
+Hack at Junction 2017.
+
+Authors: Patrik Marin, Alan Pirdil, Markus Tyrkkö, Ville Vuorenmaa
+
+### Installation
+
+```
+npm install
+cd client && npm install
+```
+
+### Development
+
+```
+npm start
+```
+
+In another terminal tab, run
+
+```
+cd client && npm start
+```
+
+### Deploy
+
+Just `git push` to master.
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  state = {
+    test: null
+  }
+
+  componentDidMount() {
+    this.fetchTest()
+  }
+
+  fetchTest = () => {
+    fetch('/api/test')
+      .then(res => res.json())
+      .then(test => this.setState({ test }))
+  }
+
   render() {
+    console.log(this.state.test)
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +28,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Return:{' '}
+          {Object.keys(this.state.test || {}).map(
+            key => this.state.test[key]
+          ) || 'null'}
+        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
