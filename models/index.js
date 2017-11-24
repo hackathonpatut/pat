9dfe2ab6b0cd4d8ad4daf10200f58db8ab23835a
Patrik Marin <me@patrikmarin.fi>
2017-11-24 21:07:29 +0200

    Create DB models

--- models/index.js
+++ models/index.js
@@ -1,14 +1,86 @@
 const Sequelize = require('sequelize')
 
 module.exports = sequelize => {
-  const applications = sequelize.define('applications', {
-    title: {
+  const applicants = sequelize.define('applicants', {
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
       type: Sequelize.STRING,
       allowNull: false
     }
   })
 
-  applications.sync()
+  applicants.sync({ force: true })
 
-  return { applications }
+  const employers = sequelize.define('employers', {
+    name: {
+      type: Sequelize.INTEGER.UNSIGNED,
+      allowNull: false
+    },
+  })
+
+  employers.sync({ force: true })
+
+  const adverts = sequelize.define('adverts', {
+    content: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+  })
+
+  adverts.sync({ force: true })
+
+  const applications = sequelize.define('applications', {
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    content: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    summary: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    filename: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  })
+
+  applications.sync({ force: true })
+
+  const resumes = sequelize.define('resumes', {
+    content: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    summary: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    filename: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  })
+
+  resumes.sync({ force: true })
+
+  const savedApplications = sequelize.define('savedApplications')
+
+  savedApplications.sync({ force: true })
+
+  employers.hasMany(adverts)
+  applicants.hasMany(applications)
+  applicants.hasOne(resumes)
+  applications.hasOne(adverts)
+  employers.belongsToMany(applications, { through: savedApplications });
+
+  sequelize.sync({ force: true })
+
+  return { applicants, applications, resumes, employers, adverts, savedApplications }
 }
